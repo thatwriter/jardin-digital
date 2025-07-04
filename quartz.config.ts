b538f88c852e777bcc88d15f20c9b80f2ef6b3a7
thatwriter <abyssdeep11@gmail.com>
2025-07-04 15:07:40 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jardín Digital",
+    pageTitle: "Jardin Digital",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
